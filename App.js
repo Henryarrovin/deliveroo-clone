5d5722cbf0e300902692871f2c08e8622d803546
Henry <henry.a.s.arrovin@gmail.com>
2023-10-23 00:58:49 +0530

    converteded from npm to yarn. Still error in yarn.

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { TailwindProvider } from "tailwindcss-react-native";
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
-import { Provider } from 'react-redux';
-import { store } from './store';
 import BasketScreen from './screens/BasketScreen';
 import PreparingOrderScreen from './screens/PreparingOrderScreen';
 import DeliveryScreen from './screens/DeliveryScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { TailwindProvider } from 'tailwindcss-react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const Stack = createNativeStackNavigator();
 
